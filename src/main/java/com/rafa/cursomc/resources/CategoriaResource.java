8c39dc72a3dc7145cd3d8f71530af4e462f149fa
Rafael note <rafaelpatricio91@gmail.com>
2019-09-25 18:30:41 -0300

    testando classe domain recursos

--- src/main/java/com/rafa/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rafa/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,37 @@
 package com.rafa.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rafa.cursomc.model.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource
 {
+	
+	
 //	@RequestMapping(method=RequestMethod.GET)
 	@GetMapping
-	public String listar()
+	public List<Categoria> listar()
 	{
-		return "REST crl";
+		Categoria c = new Categoria();
+		Categoria c2 = new Categoria();
+		c.setId(1);
+		c.setNome("Informática");
+		
+		c2.setId(2);
+		c2.setNome("Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		
+		lista.add(c);
+		lista.add(c2);
+		
+		return lista;
 	}
 }
